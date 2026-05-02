ec0836a604f996759f125bf29d21ce2f8353ac85
Jeffrey Lullen <lullenj@gmail.com>
2026-05-03 03:40:50 +0800

    Initial Commit

---
+++ MyGitJeff/MyGitJeffApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGitJeffApp.swift
+//  MyGitJeff
+//
+//  Created by jeffrey lullen on 5/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MyGitJeffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
